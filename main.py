e8c6057dc662e37867a6243546ac8ff5f9135d85
Yonatan Ben Ami <yba6081@gmail.com>
2025-09-07 16:49:34 +0300

    add: send all full metadata(metadata + path) to kafka

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from maneger import Maneger
 from get_paths import GetPaths
 from get_metadata import GetMetadata
+from send_to_kafka import SendToKafka
 
 # <<< Creating instances >>>
 maneger = Maneger()
 get_paths = GetPaths()
 get_metadata = GetMetadata()
+send_to_kafka = SendToKafka()
 
 
 directory_path = r"C:\Users\yba60\Desktop\podcasts"
 
-list_paths = get_paths.absoluteFilePaths(directory_path)
-list_metadata = maneger.get_list_metadata(list_paths)
-json_metadata = maneger.get_data_in_json(list_paths[3], list_metadata[3])
-print(json_metadata)
+list_paths = get_paths.absoluteFilePaths(directory_path) # List of all paths.
+list_metadata = maneger.get_list_metadata(list_paths) # List of all metadata.
+maneger.send_all_metadata_to_kafka(list_paths, list_metadata) # Send to kafka all full metadata.
--- maneger.py
+++ maneger.py
@@ -1,13 +1,16 @@
 from get_paths import GetPaths
 from get_metadata import GetMetadata
+from send_to_kafka import SendToKafka
 import json
 
 class Maneger:
     # <<< Creating instances >>>
     get_paths = GetPaths()
     get_metadata = GetMetadata()
+    send_to_kafka = SendToKafka()
 
     # <<< Functions >>>
+
     # Get list of paths and return list of metadata
     def get_list_metadata(self, list_paths) -> list:
         list_metadata = []
@@ -15,12 +18,13 @@ class Maneger:
             list_metadata.append(self.get_metadata.get_file_metadata(path))
         return list_metadata
     
-    # Get path and metadata, Return json file(metadata + path)
-    def get_data_in_json(self, path, metadata):
+    # Get path and metadata, Return full metadata(metadata + path)
+    def get_full_metadata(self, path, metadata):
         metadata["Path"] = path
-        json_metadata = "metadata.json"
-        with open(json_metadata, "w") as json_file:
-            json.dump(metadata, json_file, indent=4)
-            return json_file
+        return metadata
 
-        
+    def send_all_metadata_to_kafka(self, list_paths, list_metadata):
+        for i in range(len(list_paths)):
+            full_metadata = self.get_full_metadata(list_paths[i], list_metadata[i])
+            self.send_to_kafka.send(full_metadata)
+
