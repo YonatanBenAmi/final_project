7bc059d829490fdeb7621c44d57f3ac1c47beb3e
Yonatan Ben Ami <yba6081@gmail.com>
2025-09-07 16:56:46 +0300

    add: comment

--- maneger.py
+++ maneger.py
@@ -11,18 +11,19 @@ class Maneger:
 
     # <<< Functions >>>
 
-    # Get list of paths and return list of metadata
+    # Get list of paths and return list of metadata.
     def get_list_metadata(self, list_paths) -> list:
         list_metadata = []
         for path in list_paths:
             list_metadata.append(self.get_metadata.get_file_metadata(path))
         return list_metadata
     
-    # Get path and metadata, Return full metadata(metadata + path)
+    # Get path and metadata, Return full metadata(metadata + path).
     def get_full_metadata(self, path, metadata):
         metadata["Path"] = path
         return metadata
 
+    # Send all full metadata(metadata + path) to kafka.
     def send_all_metadata_to_kafka(self, list_paths, list_metadata):
         for i in range(len(list_paths)):
             full_metadata = self.get_full_metadata(list_paths[i], list_metadata[i])
